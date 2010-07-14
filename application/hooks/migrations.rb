04d9a679efbbf8b1bb9cb873006b4ed1edb37ec0
Jeremy Bush <contractfrombelow@gmail.com>
2010-07-14 17:19:22 -0500

    adding categories for initial creation

--- application/hooks/migrations.rb
+++ application/hooks/migrations.rb
@@ -14,15 +14,22 @@ DataMapper::MigrationRunner.migration 1, :create_initial_schema do
 		end
 
 		create_table :products do
-			column :id,          DataMapper::Property::Serial
-			column :name,        String, :size => 50
-			column :price,       Float
-			column :description, String, :size => 50
+			column :id,                  DataMapper::Property::Serial
+			column :name,                String, :size => 50
+			column :price,               Float
+			column :description,         String, :size => 50
+			column :product_category_id, Integer
+		end
+
+		create_table :product_categories do
+			column :id,                  DataMapper::Property::Serial
+			column :name,                String, :size => 50
 		end
 	end
 
 	down do
 		drop_table :users
 		drop_table :products
+		drop_table :product_categories
 	end
 end
