433be0a69c9efe3df9a9adbb958a7856f10ea37b
Jeremy Bush <contractfrombelow@gmail.com>
2010-07-14 18:13:31 -0500

    changing admin route to not suck, and to use the router to make it

--- application/config/routes.rb
+++ application/config/routes.rb
@@ -1,7 +1,7 @@
 Picombo::Router.add('_default', 'product/index')
 
 Picombo::Router.add('admin', lambda{ |path|
-	if Regexp.new('admin/(.+)/(.+)').match(path) # todo: make this regex match paramaters
-		{:controller => 'admin_'+Regexp.last_match[1], :method => Regexp.last_match[2], :params => []}
+	if Regexp.new('admin/(.+)').match(path)
+		Picombo::Router.process_uri('admin_'+Regexp.last_match[1])
 	end
 })
